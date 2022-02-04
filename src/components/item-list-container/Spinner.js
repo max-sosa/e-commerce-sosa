6f1ee256b66279bfb8f7a8eb74a3dd19429e61a7
max-sosa <90153004+max-sosa@users.noreply.github.com>
2022-02-04 12:53:49 -0300

    cambios en el spinner de carga

--- src/components/item-list-container/Spinner.js
+++ src/components/item-list-container/Spinner.js
@@ -2,16 +2,18 @@ import { Spinner,Button } from "react-bootstrap"
 
 const Bspinner = () =>{
     return(
-        <Button variant="success" disabled>
+        <div className="m-5">
+        <Button className="fs-1" variant="success" disabled>
             <Spinner
                 as="span"
                 animation="grow"
-                size="sm"
+                size="lg"
                 role="status"
                 aria-hidden="true"
             />
             Loading...
         </Button>
+        </div>
     )
 }
 
