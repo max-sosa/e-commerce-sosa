9aee87b1e51c989d9ee26e9cd18372e7f796d704
max-sosa <90153004+max-sosa@users.noreply.github.com>
2022-02-04 01:20:43 -0300

    adding cards

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,14 +1,15 @@
 import React from "react";
+
 import Item from "./Item";
 
  const ItemList = ({productos}) => {
-     return (
-         <div style={{display:'flex'}}>
+    return (
+        <div className="row row-cols-1 row-cols-md-2 g-4 justify-content-center" style={{display:'flex', margin:'1rem'}}>
              {
                  productos?.map((prod)=> <Item key={prod.id} prod={prod}/>)
              }
-         </div>
-     )
+        </div>
+    )
 }
 
 export default ItemList;
--- src/components/item.js
+++ src/components/item.js
@@ -3,24 +3,24 @@ import { Card, Button } from "react-bootstrap";
 import ItemCounter from "./ItemCounter";
 
 const Item = ({ prod }) => {
-  const {id, image, name, price, category,model, stock} = prod;
+  const {id, image, name, price, category, model, stock} = prod;
 
   return (
-      <Card style={{ width: '18rem', margin: '1rem'}}>
-        <Card.Img variant="top" src={image}/>
-        <Card.Body>
-            <Card.Title>{name}</Card.Title>
-            <Card.Text>
+           <Card style={{ width: '18rem', margin:'1rem'}}>
+            <Card.Img variant="top" src={image}/>
+            <Card.Body>
+              <Card.Title>{name}</Card.Title>
+              <Card.Text>
                 Categoria: {category}
                 <br></br>
                 Modelo: {model}
                 <br></br>
                 Precio:$ {price}
-            </Card.Text>
-            <Button variant="success" style={{margin:'1rem'}}>Ver detalle</Button>
-            <ItemCounter stock={stock} />
-        </Card.Body>
-        </Card>
+              </Card.Text>
+              <Button variant="success" style={{margin:'1rem'}}>Ver detalle</Button>
+              <ItemCounter stock={stock} />
+            </Card.Body>
+          </Card>
   );
 };
 
